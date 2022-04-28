938d14dbb2bf477c5031ef3a087da3e2ee3a0e47
zpg-air <327774162@qq.com>
2022-04-28 23:50:56 +0800

    [add]webpack配置dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { resolve } = require('path')
 const path = require('path')
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    // contentBase: path.join(__dirname, 'dist'),
+    static: resolve(__dirname, "build"),
+    port: '9000',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
